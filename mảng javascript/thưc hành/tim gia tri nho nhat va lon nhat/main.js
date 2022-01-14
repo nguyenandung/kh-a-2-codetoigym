f205052ff21ded071ea4b005e06e06055c59be6b
nguyenandung <txo432@gmail.com>
2022-01-14 18:03:11 +0700

    upload

--- mảng javascript/thưc hành/tim gia tri nho nhat va lon nhat/main.js
+++ mảng javascript/thưc hành/tim gia tri nho nhat va lon nhat/main.js
@@ -1,23 +1,32 @@
-// Xây dựng hàm nhận giá trị nhỏ nhất vào mảng . hàm làm nhiệm vụ tìm giá trị nhỏ nhất cyar mảng nhận vào và trả về
-// giá trị nhỏ nhất
+var nums = [];
+for(let i=0;i<100;i++){
+    nums[i] = Math.floor(Math.random()*101);
+}
 
-function findMin(arr){
-    let min = arr[0];
-    for(let i = 1 ; i<arr.length ; ++i){
-        if(arr[i]< min){
-            min = arr[i];
+function  findMin(nums){
+    let min = nums[0];
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]<min){
+            min=nums[i];
         }
     }
     return min;
 }
-
-// sử dụng hàm findmin vừa cài đặt 
-let nums =[];
-for(let i=0;i<100;++i){
-    nums[i] = Math.floor(Math.random() * 101);
-
+function findMax(nums){
+    let max = nums[0];
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>max){
+            max=nums[i];
+        }
+    }
+    return max;
 }
-let minValue = findMin(nums);
-dispArr(nums);
-document.write("<br/>")
-document.write("the minimum value is: "+ minValue);
+
+for(let i of nums){
+    document.write(i);
+}
+
+document.write('<br>');
+document.write(`Giá trị nhỏ nhất ${findMin(nums)}`)
+document.write('<br>')
+document.write(`Gia tri lon nha ${findMax(nums)}`)
